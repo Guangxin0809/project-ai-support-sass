cdd6662033d99ff06adf182827b52178858cba4b
Yu19980809 <1572250276@qq.com>
2026-04-10 13:59:21 +0800

    feat: clerk authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 
 import { useMutation, useQuery } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+import { Authenticated, Unauthenticated } from "convex/react";
 
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
-const Home = () => {
+export default function Home() {
+  return (
+    <>
+      <Authenticated>
+        <UserButton />
+        <Content />
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+    </>
+  );
+}
 
+function Content() {
   const users = useQuery(api.users.getMany);
   const addUser = useMutation(api.users.add);
 
@@ -19,7 +34,5 @@ const Home = () => {
         {JSON.stringify(users, null, 4)}
       </div>
     </div>
-  )
+  );
 }
-
-export default Home;
